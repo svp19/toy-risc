ff9f665885e1208f2cccc5ad2078fc980f17b9bd
svp19 <170010025@iitdh.ac.in>
2019-09-11 13:36:06 +0530

    some more init

--- src/generic/Simulator.java
+++ src/generic/Simulator.java
@@ -1,7 +1,10 @@
 package generic;
 
+import java.io.*;
+import java.util.Scanner;
 import processor.Clock;
 import processor.Processor;
+import processor.memorysystem.MainMemory;
 
 public class Simulator {
 		
@@ -20,7 +23,7 @@ public class Simulator {
 	{
 		/*
 		 * TODO
-		 * 1. load the program into memory according to the program layout described
+		 * 1. load the program into memory accordisg to the program layout described
 		 *    in the ISA specification
 		 * 2. set PC to the address of the first instruction in the main
 		 * 3. set the following registers:
@@ -28,6 +31,50 @@ public class Simulator {
 		 *     x1 = 65535
 		 *     x2 = 65535
 		 */
+
+		// Load program into memory
+
+		// Fetch mainMemory from processor
+		MainMemory mainMemory = processor.getMainMemory();
+
+		try
+		{
+			// Create Stream to read input file
+			FileInputStream fin = new FileInputStream(assemblyProgramFile);
+			DataInputStream dis = new DataInputStream(fin);			
+			
+			// Read PC
+			int PC = dis.readInt();
+			System.out.println(i);
+
+			// Read Int from file
+			int read4Bytes = 0, i = 0;
+			while(dis.available() > 0)
+			{
+				read4Bytes = dis.readInt();
+				mainMemory.setWord(i, read4Bytes);
+				System.out.println(read4Bytes);
+				i ++ ;
+			}
+			dis.close();
+
+			// Debugging - Print Memory
+			System.out.println(mainMemory.getContentsAsString(0, 30));
+		}
+		catch(FileNotFoundException fe)
+		{ 
+			System.out.println("FileNotFoundException : " + fe);
+		}
+		catch(IOException ioe)
+		{
+			System.out.println("IOException : " + ioe);
+		}
+
+		// Set updated memory for processor
+		processor.setMainMemory(mainMemory);
+
+		System.out.println("Nearly Done");
+
 	}
 	
 	public static void simulate()
